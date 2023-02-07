ae95d97192275afdd7183f79ef3a92cc708ca330
eslamandroid12345 <eslamemo457@gmail.com>
2023-02-07 15:52:21 +0200

    update activity

--- app/Http/Controllers/Supervisor/SupervisorController.php
+++ app/Http/Controllers/Supervisor/SupervisorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Supervisor;
 
 use App\Http\Controllers\Controller;
 use App\Models\Activity;
+use App\Models\GroupColor;
 use App\Models\GroupCustomer;
 use App\Models\GroupMovement;
 use App\Models\RouteGroup;
@@ -15,9 +16,22 @@ class SupervisorController extends Controller
 {
     public function index(){
 
-        $activities = Activity::with(['group_movements_today'])->orderBy('id', 'asc')->get();
+        $activities = Activity::with(['groups'])->orderBy('id', 'asc')->get();
 
-        $group_customers_waiting = GroupCustomer::with(['group','ticket','reservation'])->where('status','=','waiting')->whereDate('created_at','=',Carbon::now()->format('Y-m-d'))->get();
-        return view('platform.activities.index',compact('activities','group_customers_waiting'));
+//
+//        $group_customers_waiting = GroupCustomer::with(['group','ticket','reservation'])->where('status','=','waiting')
+//            ->whereDate('created_at','=',Carbon::now()->format('Y-m-d'))->get();
+
+        $group_customers_waiting = GroupColor::groupNotColored()->get();
+//
+//
+//
+//        $group_colors_active_r = Activity::with('last_active_group_movement')->get();
+
+        $group_colors_active = GroupColor::groupColored()->get();
+//        return  $group_customers_waiting;
+
+
+        return view('platform.activities.index',compact('group_customers_waiting','activities'));
     }
 }
