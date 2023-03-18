89ee4f2a0748e9f9297b567150be280240e85e3c
Abdallah Mahmoud <Abdalluhmahmoud55@gmail.com>
2023-03-18 08:58:46 +0200

    check opt

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5adebc6c948317d5c2392b91ea4c80f0
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5adebc6c948317d5c2392b91ea4c80f0', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5adebc6c948317d5c2392b91ea4c80f0', 'loadClassLoader'));
